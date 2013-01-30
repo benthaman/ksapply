8b7bf9b138d23b2ce5131e4e7408867e7aa61625
Benjamin Poirier <benjamin.poirier@gmail.com>
2013-01-30 14:32:18 -0500

    clean_header: More accurate test for -rc* difference

--- clean_header.sh
+++ clean_header.sh
@@ -168,7 +168,7 @@ header=$(echo -n "$header" | tag_extract patch-mainline)
 
 # Sometimes the tag does not include -rcX, I prefer to have it
 # var_override can take care of it, but it will generate a warning
-if echo "x$git_describe" | grep -q "x$patch_mainline"; then
+if [ "$patch_mainline" = "${git_describe%-rc*}" ]; then
 	patch_mainline=$git_describe
 fi
 
